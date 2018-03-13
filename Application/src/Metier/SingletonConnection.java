18f9bdd3591984f8025773ccb1d7d36e52966e63
Lenovo <Lenovo@Farah.mshome.net>
2018-03-13 11:18:44 +0100

    bd

---
+++ Application/src/Metier/SingletonConnection.java
@@ -0,0 +1,31 @@
+package Metier;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class SingletonConnection {
+
+private static Connection connection;
+static {
+	try {
+		Class.forName("com.mysql.jdbc.Driver");
+		connection=DriverManager.getConnection("jdbc:mysq://localhost:3306/Banque","root","");
+	}
+	catch(ClassNotFoundException ex)
+	{
+		System.out.println("Probleme de pilote base de donnees");
+		System.out.println(ex.getMessage());
+	}
+	catch(SQLException ex)
+	{
+		System.out.println("Probleme de connection ou requéte");
+		System.out.println(ex.getMessage());
+	}
+}
+public static Connection getConnection()
+{
+	return connection;
+}
+
+}
